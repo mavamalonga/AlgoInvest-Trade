f08d097ffcbd2cd16efe7c75281635a619c4f004
Alpha <mavamalonga.alpha@gmail.com>
2022-02-25 17:18:34 +0100

    solution bruteforce success!

--- bruteforce.py
+++ bruteforce.py
@@ -16,10 +16,6 @@ class BruteForce:
 			return True
 		return False
 
-	def calculate_yields(self, action):
-		yields = int(action['cout']) + int(action['cout'])*(int(action['benefice'])/100)
-		return yields
-
 	def check_actions_purchased(self, action, actions_purchased):
 		for action_purchased in actions_purchased:
 			if action['action'] == actions_purchased['action']:
@@ -33,33 +29,59 @@ class BruteForce:
 					yields = self.calculate_yields(action)
 					actions_purchased.append(action)
 
-	def try_all_possibilities_for_value(self):
-		your_list = 'abcd'
-		complete_list = []
-		for current in range(4):
-			a = [i for i in your_list]
-			for y in range(current):
-				a = [x+i for i in your_list for x in a if i not in x]
-			complete_list = complete_list+a
-		print(complete_list)
+	def calculate_group_actions_cost(self, group_action):
+		total_cost = 0
+		actions_number = group_action.split("$")
+		for nb in actions_number:
+			for action in self.actions:
+				if action['action'] == f'action-{nb}':
+					total_cost = total_cost + int(action['cout'])
+		return total_cost
+
+	def calculate_yields(self, group_action):
+		actions_number = group_action.split("$")
+		yields = 0
+		for nb in actions_number:
+			for action in self.actions:
+				if action['action'] == f'action-{nb}':
+					yields = yields + int(action['cout']) + int(action['cout'])*(int(action['benefice'])/100)
+		return yields
+
+	def action_not_in_group(self, new_action, group_action):
+		actions_number = group_action.split("$")
+		return new_action not in actions_number
 
 	def algo_brute_force(self):
 		your_list = self.actions
 		complete_list = []
-		propositions = []
+		max_yields = 0
 		a_bis = []
-
 		for current in range(len(your_list)):
 			i_index = your_list.pop(0)
 			your_list.append(i_index)
 			a = [i['action'][7:] for i in self.actions]
+			e=0
 			for i in your_list:
 				for x in a:
-					val = i['action'][7:]+ "$" + x
-					a_bis.append(val)
+					action_not_in = self.action_not_in_group(i['action'][7:], x)
+					if action_not_in:
+						add_new_action = i['action'][7:]+ "$" + x
+						total_cost = self.calculate_group_actions_cost(add_new_action)
+						if total_cost < 500:
+							val = i['action'][7:]+ "$" + x
+							a_bis.append(val)
+						else:
+							val = x
+							a_bis.append(val)
+							yields = self.calculate_yields(x)
+							if yields > max_yields:
+								max_yields = yields
+								print({"group_actions": val, "yields_max": max_yields,})
+				e = e + 1
+				print(f"tour : {e}")
 				a = [c for c in a_bis]
-				print(a)
-				exit()
+			print(f"all for {i}")
+			exit()
 			#complete_list = complete_list+a
 
 	def main(self):
