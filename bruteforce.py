7f1dbb6ee43c679aa9c69a6f5916cdd8499eeeec
Alpha <mavamalonga.alpha@gmail.com>
2022-03-01 17:47:31 +0100

    bruteforce

--- bruteforce.py
+++ bruteforce.py
@@ -1,4 +1,6 @@
 from data import actions, sold
+import yaml
+import json
 
 
 class BruteForce:
@@ -6,27 +8,20 @@ class BruteForce:
 		self.actions = actions
 		self.sold = sold
 
-	def calculate_cost(self, group_action):
+	def calculate_cost(self, group):
 		total_cost = 0
-		actions_number = group_action.split("$")
-		for nb in actions_number:
-			for action in self.actions:
-				if action['action'] == f'action-{nb}':
-					total_cost = total_cost + int(action['cout'])
+		for action in group:
+			total_cost = total_cost + int(action['cout'])
 		return total_cost
 
-	def calculate_yields(self, group_action):
-		numbers = group_action.split("$")
+	def calculate_yields(self, groups):
 		yields = 0
-		for nb in numbers:
-			for action in self.actions:
-				if action['action'] == f'action-{nb}':
-					yields = yields + int(action['cout']) + int(action['cout'])*(int(action['benefice'])/100)
+		for action in groups:
+			yields = yields + int(action['cout']) + int(action['cout'])*(int(action['benefice'])/100)
 		return yields
 
-	def check_action_not_in_group(self, new_action, group_action):
-		actions_number = group_action.split("$")
-		return new_action not in actions_number
+	def check_action_not_in_group(self, action, group):
+		return action not in group
 
 	def main(self):
 		actions = self.actions
@@ -34,28 +29,31 @@ class BruteForce:
 		group_actions_bis = []
 
 		for current in range(len(actions)):
-			index = actions.pop(0)
-			actions.append(index)
-			group_actions = [a['action'][7:] for a in self.actions]
+			group_actions = [[a] for a in self.actions]
 			for action in actions:
 				for group in group_actions:
-					action_not_in_group = self.check_action_not_in_group(action['action'][7:], group)
+					action_not_in_group = self.check_action_not_in_group(action, group)
 					if action_not_in_group:
-						new_group = group + "$" + action['action'][7:]
-						total_cost = self.calculate_cost(new_group)
+						group.append(action)
+						total_cost = self.calculate_cost(group)
 						if total_cost < self.sold:
-							new_group = group + "$" + action['action'][7:]
-							group_actions_bis.append(new_group)
+							group_actions_bis.append(group)
 						else:
+							group.pop(-1)
 							group_actions_bis.append(group)
 							yields = self.calculate_yields(group)
 							if yields > max_yields:
 								max_yields = yields
-								print({"group_actions": group, "yields_max": max_yields})
+					else:
+						group_actions_bis.append(group)
 				group_actions = [group for group in group_actions_bis]
+			index = actions.pop(0)
+			actions.append(index)
+		return {"actions": group, "yields": max_yields}
 
 
 if __name__ == '__main__':
 	bf = BruteForce(actions, sold)
-	bf.main()
+	max_yield = bf.main()
+	print(max_yield)
 		
