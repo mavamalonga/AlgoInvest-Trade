05b6e578ee60ec79869f8f4c9433c8a972fa50b1
Alpha <mavamalonga.alpha@gmail.com>
2022-03-02 17:00:21 +0100

    upgrade main method

--- bruteforce.py
+++ bruteforce.py
@@ -23,37 +23,45 @@ class BruteForce:
 	def check_action_not_in_group(self, action, group):
 		return action not in group
 
+	def check_other_elem_can_add(self, group, total_cost):
+		for action in self.actions:
+			action_not_in_group = self.check_action_not_in_group(action, group)
+			if total_cost + int(action['cout']) < self.sold and action_not_in_group:
+				return True
+		return False
+
 	def main(self):
 		actions = self.actions
 		max_yields = 0
-		group_actions_bis = []
 
 		for current in range(len(actions)):
 			group_actions = [[a] for a in self.actions]
+			group_actions_bis = []
+			print(f"combinations index action : {current}")
 			for action in actions:
 				for group in group_actions:
-					action_not_in_group = self.check_action_not_in_group(action, group)
-					if action_not_in_group:
+					if self.check_action_not_in_group(action, group):
 						group.append(action)
 						total_cost = self.calculate_cost(group)
 						if total_cost < self.sold:
 							group_actions_bis.append(group)
 						else:
 							group.pop(-1)
-							group_actions_bis.append(group)
-							yields = self.calculate_yields(group)
-							if yields > max_yields:
-								max_yields = yields
+							if self.check_other_elem_can_add(group, total_cost):	
+								group_actions_bis.append(group)
+							else:
+								yields = self.calculate_yields(group)
+								if yields > max_yields:
+									max_yields = yields
+									print({"action":f"{action}", "yields": max_yields})
 					else:
 						group_actions_bis.append(group)
 				group_actions = [group for group in group_actions_bis]
 			index = actions.pop(0)
 			actions.append(index)
-		return {"actions": group, "yields": max_yields}
 
 
 if __name__ == '__main__':
 	bf = BruteForce(actions, sold)
 	max_yield = bf.main()
-	print(max_yield)
 		
