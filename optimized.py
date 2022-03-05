ac26f0eed98c213f3a715fb4c4d4fe260c544aff
Alpha <mavamalonga.alpha@gmail.com>
2022-03-05 15:38:55 +0100

    optimized v1

--- optimized.py
+++ optimized.py
@@ -16,9 +16,9 @@ def knapSack(W, wt, val, n):
 	return K[n][W]
 """
 #Main
-val = [50,100,150,200,10] #un tableau de valeurs qui contient la valeur de tous les Ã©lÃ©ments <=>RENDEMENT
-wt = [8,16,32,40,10] #nous avons un tableau de poids qui contient le poids de tous les Ã©lÃ©ments <=> COUT
-W = 74 #nous avons une capacitÃ© de poids totale du sac Ã  dos <=> SOLDE 
+val = [50,100,150,200,10] #un tableau de valeurs qui contient la valeur de tous les éléments <=>RENDEMENT
+wt = [8,16,32,40,10] #nous avons un tableau de poids qui contient le poids de tous les éléments <=> COUT
+W = 74 #nous avons une capacité de poids totale du sac à dos <=> SOLDE 
 n = len(val)
 print(knapSack(W, wt, val, n))
 """
